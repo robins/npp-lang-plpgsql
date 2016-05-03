d66e58edec16c4e6be3d7f7049e7097e0d2539ce
Robins Tharakan <tharakan@gmail.com>
2016-05-03 18:44:35 +0530

    updated script to fetch config params as well. Also make the scripts a little better presentable and generate text files. Next commit should be about text files thus generated (As a snapshot)

--- pgdoc_parse.sh
+++ pgdoc_parse.sh
@@ -1,7 +1,14 @@
+# XXX: Remove empty lines at the end of generated text files
+
 # Fetch reserved words from PG Docs - DEVEL branch
 # XXX: This currently fetches ALL in the list, and doesn't filter those that are blank / non-reserved in PG column
 
- curl -so - http://www.postgresql.org/docs/devel/static/sql-keywords-appendix.html  | grep -A2 "TOKEN" | tr -d '\n' | sed 's/--/\n/g' | grep -v "non-reserved" | grep "reserved" | sed 's/\</ /g' | sed 's/\>/ /g' | awk '{print $9}' | grep -v "<" | grep -v ">" > reserved_words.txt
+ curl -so - \
+  http://www.postgresql.org/docs/devel/static/sql-keywords-appendix.html  \
+    | grep -A2 "TOKEN" | tr -d '\n' | sed 's/--/\n/g' | grep -v "non-reserved" \
+    | grep "reserved" | sed 's/\</ /g' | sed 's/\>/ /g' | awk '{print $9}' \
+    | grep -v "<" | grep -v ">" \
+      > reserved_words.txt
 
 
  
@@ -26,4 +33,26 @@
   http://www.postgresql.org/docs/devel/static/datatype-oid.html \
   http://www.postgresql.org/docs/devel/static/datatype-pg-lsn.html \
   http://www.postgresql.org/docs/devel/static/datatype-pseudo.html \
-    | grep "TYPE" | grep td | sed 's/\</ /g' | sed 's/\>/ /g' | awk '{print $9}' | grep -v "<" | grep -v ">" | grep -v "XHTML" | sort | uniq > datatypes.txt
+    | grep "TYPE" | grep td | sed 's/\</ /g' | sed 's/\>/ /g' \
+    | awk '{print $9}' | grep -v "<" | grep -v ">" | grep -v "XHTML" \
+    | sort | uniq \
+      > datatypes.txt
+
+    
+curl -so -  \
+  http://www.postgresql.org/docs/devel/static/runtime-config-connection.html     \
+  http://www.postgresql.org/docs/devel/static/runtime-config-resource.html       \
+  http://www.postgresql.org/docs/devel/static/runtime-config-wal.html            \
+  http://www.postgresql.org/docs/devel/static/runtime-config-replication.html    \
+  http://www.postgresql.org/docs/devel/static/runtime-config-query.html          \
+  http://www.postgresql.org/docs/devel/static/runtime-config-logging.html        \
+  http://www.postgresql.org/docs/devel/static/runtime-config-statistics.html     \
+  http://www.postgresql.org/docs/devel/static/runtime-config-autovacuum.html     \
+  http://www.postgresql.org/docs/devel/static/runtime-config-client.html         \
+  http://www.postgresql.org/docs/devel/static/runtime-config-locks.html          \
+  http://www.postgresql.org/docs/devel/static/runtime-config-compatible.html     \
+  http://www.postgresql.org/docs/devel/static/runtime-config-error-handling.html \
+  http://www.postgresql.org/docs/devel/static/runtime-config-preset.html         \
+  http://www.postgresql.org/docs/devel/static/runtime-config-developer.html      \
+  | grep -oP -i 'varname.{0,40}' | cut --bytes=10- | cut -d "<" -f1 | sort| uniq \
+    > configuration_parameters.txt
